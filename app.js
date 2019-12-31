96d012692ef9aab60df73be8dd64e817cf5fedd0
Mohit Juneja <mohit0920@icloud.com>
2019-12-31 21:32:07 +0530

    successfully extracting public ip for linux or unix system like mac ubuntu

--- app.js
+++ app.js
@@ -8,11 +8,19 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(__dirname + "/public"))
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}));
-
-
+const { exec } = require('child_process');
 
 app.get("/", (req,res) => {
-	res.send("Connected YOOO!! Hurray ")
+	exec(' ifconfig | grep 192 | cut  -d" " -f2 ', (error, ip, stderr) => {
+  		if (error) {
+    		console.error(`exec error: ${error}`);
+    		return;
+ 	 }
+//  	console.log(`stdout: ${ip}`);
+//  	console.error(`stderr: ${stderr}`);
+	res.send("Connected YOOO!! Hurray "+ip)
+	});
+
 })
 app.get("/:pagename", (req,res) => {
 	res.sendFile(path.join(__dirname +"/"+ req.params.pagename));
