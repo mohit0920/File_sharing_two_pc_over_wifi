d488e5c66306cfbad2a3905e531fff246b87bc74
Mohit Juneja <mohit0920@icloud.com>
2020-04-01 13:10:53 +0530

    upload for sending files from client to server

--- app.js
+++ app.js
@@ -4,6 +4,23 @@ const app = express()
 const path = require('path')
 const fs = require('fs')
 const directoryPath = path.join(__dirname, 'Share');
+var multer  = require('multer')
+//var upload = multer({ dest: 'uploads/' })
+
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
+ 
+var upload = multer({ storage: storage })
+
+
+
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
@@ -12,12 +29,25 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}));
 const { exec } = require('child_process');
 
+
+
+
+app.post('/upload', upload.single('uf'), function (req, res, next) {
+  // req.file is the `avatar` file
+  // req.body will hold the text fields, if there were any
+	//var file = req.file
+	res.send("File uploaded. Please reload page to upload another.");
+})
+
+
+
+
 app.get("/", (req,res) => {
-	exec(' ifconfig | grep 192 | cut  -d" " -f2 ', (error, ip, stderr) => {
-  		if (error) {
-    		console.error(`exec error: ${error}`);
-    		return;
- 	 }
+//	exec(' ifconfig | grep 192 | cut  -d" " -f2 ', (error, ip, stderr) => {
+  //		if (error) {
+    //		console.error(`exec error: ${error}`);
+    	//	return;
+ 	// }
 
 //  	console.log(`stdout: ${ip}`)
   	// console.error(`stderr: ${stderr}`)
@@ -35,11 +65,12 @@ app.get("/", (req,res) => {
 		files_to_share=files
 //    		});
 			
-		var data = {ip: ip.substring(0, ip.length-1), files: files_to_share}
+	//	var data = {ip: ip.substring(0, ip.length-1), files: files_to_share}
+		var data = {ip: "ip", files: files_to_share}
 	res.render('index', {data: JSON.stringify(data)})
 	//res.send("Connected YOOO!! Hurray "+ip+JSON.stringify(files_to_share))
 	});
-	})
+//	})
 
 })
 app.get("/:pagename", (req,res) => {
